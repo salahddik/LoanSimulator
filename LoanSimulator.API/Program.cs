9d1539935aa40df2e91209277de088b574021cc7
salah eddine dik <zettodik@gmail.com>
2025-08-02 01:40:36 +0100

    Add Swagger support and set up project references for layered architecture
    - Integrated Swagger/in LoanSimulator.API for API documentation
    - Added project references to establish layered architecture dependencies:
      * LoanSimulator.API references LoanSimulator.Application
      * LoanSimulator.Application references LoanSimulator.Infrastructure
      * LoanSimulator.Tests references API, Application, and Infrastructure projects

--- LoanSimulator.API/Program.cs
+++ LoanSimulator.API/Program.cs
@@ -1,28 +1,27 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+
+// Add Swagger services
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
-
-app.UseDefaultFiles();
-app.MapStaticAssets();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
+app.UseDefaultFiles();
+app.MapStaticAssets();
+
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.MapFallbackToFile("/index.html");
 
 app.Run();
