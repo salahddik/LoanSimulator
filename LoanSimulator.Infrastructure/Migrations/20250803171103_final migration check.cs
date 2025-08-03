c37948e5f657d7737e82702fad21c23bfd8acf30
salah eddine dik <zettodik@gmail.com>
2025-08-03 18:12:12 +0100

    feat(database): add final migration check
    - Added a new EF Core migration named "final migration check"
    - Updates database schema to latest state based on current models
    - Prepares database for upcoming features and fixes

---
+++ LoanSimulator.Infrastructure/Migrations/20250803171103_final migration check.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LoanSimulator.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class finalmigrationcheck : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
